f28afedc6f961470d273bccf8132619bdb0872a9
Yannick <chronostimeincahin@gmail.com>
2025-12-16 09:42:01 -0300

    Concluído funcionamento do programa para Windows de Linux

---
+++ copiararquivos.spec
@@ -0,0 +1,28 @@
+# copiararquivos.spec
+block_cipher = None
+
+a = Analysis(
+    ['copiararquivos.py'],   # arquivo principal
+    pathex=[],
+    binaries=[],
+    datas=[
+    ],
+    hiddenimports=['metodos'],  # módulos auxiliares
+    hookspath=[],
+    runtime_hooks=[],
+    excludes=[],
+    cipher=block_cipher,
+)
+pyz = PYZ(a.pure, a.zipped_data, cipher=block_cipher)
+exe = EXE(
+    pyz,
+    a.scripts,
+    a.binaries,
+    a.zipfiles,
+    a.datas,
+    name='copiararquivos',
+    debug=False,
+    strip=False,
+    upx=True,
+    console=False,  # sem janela de prompt
+)
--- metodos.py
+++ metodos.py
@@ -1,4 +1,178 @@
-def copiar_arquivos(texto_origem, texto_destino):
-    print("testando execução")
-    print(texto_origem)
-    print(texto_destino)
+import time
+from pathlib import Path
+import threading
+import shutil
+from tkinter import filedialog, messagebox
+
+cancelar = False
+def parar_copia():
+    global cancelar
+    cancelar = True
+
+def copiando_arquivos(texto_origem,
+                      texto_destino,
+                      root,
+                      progress_canvas,
+                      entrada_origem,
+                      entrada_destino,
+                      button_executar_copia,
+                      text_area,
+                      label_copiado_contagem,
+                      label_tempo_decorrido,
+                      checkbox_origem,
+                      checkbox_encerrar):
+    global cancelar
+
+    pasta_matriz = str(texto_origem).split("/")
+
+    origem = Path(texto_origem)
+    if checkbox_origem:
+        texto_destino = f"{texto_destino}/{pasta_matriz[len(pasta_matriz) - 1]}"
+        #print(verificar)
+
+    destino = Path(texto_destino)
+
+    #print(pasta_matriz[len(pasta_matriz) -1])
+    destino.mkdir(parents=True, exist_ok=True)
+
+    #entrada_origem.config(state="disabled")
+    #entrada_destino.config(state="disabled")
+    #button_executar_copia.config(state="disabled")
+
+    # desabilita entradas e botão
+    root.after(0, lambda: entrada_origem.config(state="disabled"))
+    root.after(0, lambda: entrada_destino.config(state="disabled"))
+    root.after(0, lambda: button_executar_copia.config(state="disabled"))
+
+    # lista todos os arquivos e subpastas
+    arquivos = list(origem.rglob("*"))
+    total = len(arquivos)
+
+    #progress_bar["maximum"] = total  # define o valor máximo da barra
+    #progress_bar["value"] = 0  # inicia em zero
+
+    tamanho_item = 0
+    inicio = time.time() # marca o início da execução
+    for i, item in enumerate(arquivos, start=1):
+        if cancelar:
+            text_area.delete(1.0, "end") # apaga tudo
+            progress_canvas.delete("all")
+            cancelar = False
+            break
+
+        destino_item = destino / item.relative_to(origem)
+        try:
+            if item.is_dir():
+                destino_item.mkdir(parents=True, exist_ok=True)
+            else:
+                destino_item.parent.mkdir(parents=True, exist_ok=True)
+                shutil.copy2(item, destino_item)
+                tamanho_item += item.stat().st_size
+                text_area.delete("1.0", "end")  # apaga tudo
+                text_area.insert("1.0", item)
+                label_copiado_contagem.config(text=formatar_tamanho(tamanho_item))
+
+        except Exception as e:
+            # Mostra o erro mas continua
+            print(f"Erro ao copiar {item}: {e}")
+
+        # tempo decorrido
+        decorrido = time.time() - inicio
+        # label_tempo_decorrido.config(text=f"{decorrido:.1f} s")
+        minutos, segundos = divmod(decorrido, 60)
+        label_tempo_decorrido.config(text=f"{int(minutos)}:{segundos:04.1f}")
+
+        atualizar_barra(i, total, progress_canvas)
+
+        # atualiza a barra de progresso
+        #progress_bar["value"] = i
+        root.update_idletasks()  # força atualização da interface
+
+    text_area.insert("2.0", "\nFinalizado!")
+
+    entrada_origem.config(state="enabled")
+    entrada_destino.config(state="enabled")
+    button_executar_copia.config(state="enabled")
+
+    if checkbox_encerrar:
+        messagebox.showinfo("Encerrando", "Programa será encerrado")
+        root.destroy()
+
+### Atualiza a barra de progresso ###
+def atualizar_barra(valor, total, progress_canvas):
+    progress_canvas.delete("all")
+    largura = int((valor / total) * 300)
+    # desenha a barra preenchida
+    progress_canvas.create_rectangle(0, 0, largura, 25, fill="green")
+    # escreve a porcentagem dentro da barra
+    porcentagem = (valor / total) * 100
+    progress_canvas.create_text(150, 12, text=f"{porcentagem:.3f}%", fill="black", font=("Arial", 10, "bold"))
+
+def selecionar_pasta():
+    pasta = filedialog.askdirectory(title="Selecione uma pasta")
+    if pasta:  # se o usuário não cancelar
+        return pasta
+    else:
+        return None
+def formatar_tamanho(tamanho):
+    tamanho_kb = tamanho / 1024
+    tamanho_mb = tamanho / 1024 / 1024
+    tamanho_gb = tamanho / 1024 / 1024 / 1024
+
+    if tamanho > (1024 * 1024 * 1024):
+        return f"{tamanho_gb:.2f} GiB"
+    elif tamanho > (1024 * 1024):
+        return f"{tamanho_mb:.2f} MB"
+    elif tamanho > 1024:
+        return f"{tamanho_kb:.2f} KB"
+    else:
+        return f"{tamanho:.2f} B"
+
+def tamaho_pasta(entrada_origem, label_tamanho_contagem):
+    origem = Path(entrada_origem)
+    arquivos = list(origem.rglob("*"))
+
+    tamanho_total = sum(f.stat().st_size for f in arquivos if f.is_file())
+
+    label_tamanho_contagem.config(text=formatar_tamanho(tamanho_total))
+
+def inciar_contagem(entrada_origem,
+                    label_tamanho_contagem):
+    t = threading.Thread(
+        target=tamaho_pasta,
+        args=(entrada_origem,
+              label_tamanho_contagem),
+        daemon=True
+    )
+    t.start()
+
+# --- Inicia a cópia --- #
+def iniciar_copia(texto_origem,
+                      texto_destino,
+                      root,
+                      progress_canvas,
+                      entrada_origem,
+                      entrada_destino,
+                      button_executar_copia,
+                      text_area,
+                  label_copiado_contagem,
+                  label_tempo_decorrido,
+                  checkbox_origem,
+                  checkbox_encerrar):
+    t = threading.Thread(
+        target=copiando_arquivos,
+        args=(texto_origem,
+              texto_destino,
+              root,
+              progress_canvas,
+              entrada_origem,
+              entrada_destino,
+              button_executar_copia,
+              text_area,
+              label_copiado_contagem,
+              label_tempo_decorrido,
+              checkbox_origem,
+              checkbox_encerrar),
+        daemon=True
+    )
+    t.start()
