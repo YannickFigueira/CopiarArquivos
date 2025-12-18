f48fd89f73f4b3868c319e0882cbba756fcc27ac
Yannick Figueira <chronostimeinchain@gmail.com>
2025-12-18 18:27:40 -0300

    Adicionado link para download da nova versão

--- verificarversao.py
+++ verificarversao.py
@@ -1,3 +1,4 @@
+import webbrowser
 from tkinter import messagebox
 
 import requests
@@ -8,12 +9,21 @@ def consultar_lancamento(repo, version):
     #repo = "CopiarArquivos"
 
     url = f"https://api.github.com/repos/{owner}/{repo}/releases/latest"
+    pagina = f"https://github.com/{owner}/{repo}/releases"
 
     response = requests.get(url)
     if response.status_code == 200:
         release = response.json()
         if not release["tag_name"] == version:
-            messagebox.showinfo("Lancamento", f"Nova versão\n{release['name']}")
+            #messagebox.showinfo("Lancamento", f"Nova versão\n{release['name']}")
+
+            resposta = messagebox.askyesno(
+                "Lançamento",
+                f"Nova versão: {release['name']}\n\nDeseja abrir o link de download?"
+            )
+
+            if resposta:
+                webbrowser.open(pagina)
             #print("Última versão:", release["tag_name"])
             #print("Nome:", release["name"])
             #print("Publicado em:", release["published_at"])
